953052e44188388c0dd4a3325cbaa72486a33806
DanielePalaia <dpalaia@pivotal.io>
2021-01-14 17:53:56 +0100

    commit

--- jsonparser.py
+++ jsonparser.py
@@ -1,27 +1,40 @@
 import json
 
+# json keys to search
+superkey = "repeatedMessages"
+entry = "Usage"
+
+# input and output files
 with open('./gpsscli_file.yaml') as input:
     datainput = input.read()
 
-with open('./customer.json') as f:
+with open('./usage.json') as f:
     data = json.load(f)
 
-data = data['repeatedMessages']
+data = data[superkey]
 
 line = ""
-for item in data['Customer']:
-    for key in item.keys():
+
+# looping
+for item in data['Usage']:
+    for key  in item.keys():
+        value = item[key]
         line1 = "        - NAME: " + key
-        if "Time" and "Date" not in key:
-            line2 = "          EXPRESSION: json_array_elements(jdata->'repeatedMessages'->'Customer')->>'" + key + "'::varchar as " + key
+        if  ("Time" in key or "Date" in key or "time" in key or "date" in key):
+            line2 = "          EXPRESSION: to_timestamp((json_array_elements(jdata->'"+superkey+"'->'"+entry+"')->>'" + key + "')::double precision/1000) as " + key
+        elif (isinstance(value, float)):
+            line2 = "          EXPRESSION: (json_array_elements(jdata->'"+superkey+"'->'"+entry+"')->>'" + key + "')::numeric as " + key
+        elif (isinstance(value, int)):
+            line2 = "          EXPRESSION: (json_array_elements(jdata->'"+superkey+"'->'"+entry+"')->>'" + key + "')::bigint as " + key
         else:
-            line2 = "          EXPRESSION: to_timestamp((json_array_elements(jdata->'repeatedMessages'->'Customer')->>'" + key + "')::double precision/1000) as " + key
+            line2 = "          EXPRESSION: (json_array_elements(jdata->'"+superkey+"'->'"+entry+"')->>'" + key + "')::text as " + key
 
         line += line1 + "\n"+ line2 + "\n"
 
+# concatenation of results fon final file
 output = datainput + "\n" + line + "\n"
 output = output + "   COMMIT:" + "\n"
-output = output + "     MAX_ROW:1"  + "\n"
-output = output + "     MINIMAL_INTERVAL:1000"
+output = output + "     MAX_ROW: 1"  + "\n"
+output = output + "     MINIMAL_INTERVAL: 1000"
 
 print output
