3d59c86ef303410cacd33f1fc75ff2f4d09ef8f4
DanielePalaia <dpalaia@pivotal.io>
2021-02-03 15:06:27 +0100

    udpating script

--- pythonparser/jsonparser.py
+++ pythonparser/jsonparser.py
@@ -1,4 +1,5 @@
 import json
+import string
 
 # input files
 gpsscli_configuration_file = "./gpsscli_file.yaml"
@@ -20,21 +21,30 @@ def main():
 
     data = data[superkey]
 
-    line = ""
+    line1 = "     UPDATE_COLUMNS:"
+    for item in data[entry]:
+        for key  in item.keys():
+            value = item[key]
+            line1 = line1 + "\n" + "        - " + string.lower(key)
+            
+
+    line = line1 + '\n'
+    # mapping
+    line =  line + "     MAPPING:\n"
 
     # looping
     for item in data[entry]:
         for key  in item.keys():
             value = item[key]
-            line1 = "        - NAME: " + key
+            line1 = "        - NAME: " + string.lower(key)
             if  ("Time" in key or "Date" in key or "time" in key or "date" in key):
-                line2 = "          EXPRESSION: to_timestamp((json_array_elements(jdata->'"+superkey+"'->'"+entry+"')->>'" + key + "')::double precision/1000) as " + key
+                line2 = "          EXPRESSION: to_timestamp((json_array_elements(jdata->'"+superkey+"'->'"+entry+"')->>'" + key + "')::double precision/1000)" 
             elif (isinstance(value, float)):
-                line2 = "          EXPRESSION: (json_array_elements(jdata->'"+superkey+"'->'"+entry+"')->>'" + key + "')::numeric as " + key
+                line2 = "          EXPRESSION: (json_array_elements(jdata->'"+superkey+"'->'"+entry+"')->>'" + key + "')::numeric" 
             elif (isinstance(value, int)):
-                line2 = "          EXPRESSION: (json_array_elements(jdata->'"+superkey+"'->'"+entry+"')->>'" + key + "')::bigint as " + key
+                line2 = "          EXPRESSION: (json_array_elements(jdata->'"+superkey+"'->'"+entry+"')->>'" + key + "')::bigint" 
             else:
-                line2 = "          EXPRESSION: (json_array_elements(jdata->'"+superkey+"'->'"+entry+"')->>'" + key + "')::text as " + key
+                line2 = "          EXPRESSION: (json_array_elements(jdata->'"+superkey+"'->'"+entry+"')->>'" + key + "')::text" 
 
             line += line1 + "\n"+ line2 + "\n"
 
